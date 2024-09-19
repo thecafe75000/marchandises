3555e775bb565d7f0476a607d475a80943a24b51
LEI <lei@Laptop-LEI-MAC.local>
2024-09-19 21:33:41 +0200

    a new function for changing price

--- js/index.js
+++ js/index.js
@@ -308,6 +308,9 @@ window.onload = function () {
         var ddNode = document.createElement('dd')
         ddNode.innerText = subitem.type
 
+        // 给dd元素添加一个自定义的属性
+        ddNode.setAttribute('price', subitem.changePrice)
+
         // dl里追加dd
         dlNode.appendChild(ddNode)
       })
@@ -321,7 +324,7 @@ window.onload = function () {
 
 
   // 点击商品参数之后的颜色排他效果
-  function clickbbBind() {
+  function clickddBind() {
     // 获取所有的dl元素, 取其中第1个dl元素下的所有dd先做测试,测试完毕后在dl里嵌套一个循环
     var dlNodes = document.querySelectorAll(
       '#wrapper #content .contentMain .center .right .rightBottom .choseWrap dl'
@@ -332,8 +335,8 @@ window.onload = function () {
       '#wrapper #content .contentMain .center .right .rightBottom .choose'
     )
 
-    // 首先创建一个可容纳点击的dd元素值的容器即一个数组, 确定数组的起始长度并初始化值为null
-    var arr = new Array(dlNodes.length).fill(null)
+    // 首先创建一个可容纳点击的dd元素值的容器即一个数组, 确定数组的起始长度并初始化值为0
+    var arr = new Array(dlNodes.length).fill(0)
     // console.log(arr)
 
     Array.from(dlNodes).map((dlitem,dlindex) => {
@@ -353,22 +356,27 @@ window.onload = function () {
           this.style.color = '#cc1122'
           // console.log('this', this)
 
-          // 点击哪一个dd元素动态产生一个新的mark标记元素
-          // 然后再将点击的dd元素的值按照对应下标来写入到数组的元素身上
-          arr[dlindex] = this.innerText
+          // 点击的那个dd元素就动态产生一个新的mark标记元素
+          arr[dlindex] = this
+
+          // 调用价格变化的函数
+          changePricefn(arr)
 
           // 遍历arr数组,将非0元素的值写入到mark标记中
-          arr.forEach((value) => {
+          arr.forEach((value, arridx) => {
             // 只要有value, 就动态创建mark标签
             if (value) {
               // 创建div元素,并且设置其class属性为mark
               var markDiv = document.createElement('div')
               markDiv.className = 'mark'
-              markDiv.innerText = value
-
+              markDiv.innerText = value.innerText
+              
               // 创建a元素,并且设置其值为X
               var aNode = document.createElement('a')
               aNode.innerText = 'X'
+
+              // 给a元素添加一个属性为下标方便后面针对性的删除
+              aNode.setAttribute('aindex', arridx)
 
               // 给div里追加a
               markDiv.appendChild(aNode)
@@ -377,12 +385,76 @@ window.onload = function () {
               choose.appendChild(markDiv)
             }
           })
+
+          // 获取所有a标签元素，并且循环发生点击事件
+          var aNodes = document.querySelectorAll(
+            '#wrapper #content .contentMain .center .right .rightBottom .choose .mark a'
+          )
+          
+          Array.from(aNodes).map((aitem) => {
+            aitem.onclick = function () {
+              // 获取被点击的a标签的下标属性值,属性名要和上面 aNode.setAttribute('aindex', arridx)一致
+              var aidx = this.getAttribute('aindex')
+              
+              // 恢复数组中对应下标元素的值
+              arr[aidx] = 0
+              
+              // 查找对应下标的那个dl行中的所有dd元素
+              var ddlist = dlNodes[aidx].querySelectorAll('dd')
+              
+              // 遍历所有的dd元素
+              Array.from(ddlist).map((dditem) => {
+                // 排他思想, 所有dd元素的文字为灰色
+                dditem.style.color = '#666'
+              })
+
+              // 默认第1个dd元素恢复成红色
+              ddlist[0].style.color = '#cc1122'
+
+              // 删除对应下标位置的.mark的元素，从其父元素那里删除即可
+              choose.removeChild(this.parentNode)
+
+              // 调用价格函数
+              changePricefn(arr)
+
+            }
+            
+          })
+
         }
       })
+
     })
   }
+  clickddBind()
 
-  clickbbBind()
+
+  // 价格变动的函数, 当点击dd元素和删除.mark的div元素时调用该函数
+  function changePricefn(arr) {
+    // 获取原价格的标签元素
+    var oldPrice = document.querySelector(
+      '#wrapper #content .contentMain .center .right .rightTop .priceWrap .priceTop .price p'
+    )
+
+    // 取出默认的价格
+    var price = goodData.goodsDetail.price
+
+    // 遍历arr数组，将dd元素身上的新变化的价格和原本已有的价格5299相加
+    arr.map((item) => {
+      if (item) {
+        // 数据源里的数据类型是字符串，需要做数据类型转换
+        var changeprice = Number(item.getAttribute('price'))
+
+        // 最终价格
+        price = price + changeprice
+      }
+      
+    })
+
+    // 最后将计算之后的结果重新渲染到p标签里
+    oldPrice.innerText = price
+
+  }
 
  
 }
